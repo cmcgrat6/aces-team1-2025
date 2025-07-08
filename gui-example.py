388681a55d45613edbf1f1f59c5353571896c856
cmcgrat6 <cmcgrat6@jaguarlandrover.com>
2025-07-08 16:34:48 +0100

    Updated speech and gui-example, edited images
    speech.py now has more commands, but for ease of testing each speech input is now processed through the command line. Remove the "input() #" from affected lines to fix this.
    Made the GUI look better and added a radio menu and a basic trip computer that updates every second.
    speech.py now hooks into the GUI, meaning both can be used at the same time and speech.py talks to the GUI and can change things about it.

--- gui-example.py
+++ gui-example.py
@@ -1,14 +1,36 @@
-# run pip install pyqt6
+# run pip install pyqt6 SpeechRecognition pyttsx3 PyAudio pycaw screen-brightness-control
+# press any button to go to its specific screen 
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QLineEdit, QGridLayout, QStackedLayout
-from PyQt6.QtWidgets import QSlider
+from PyQt6.QtWidgets import QApplication, QSlider, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QLineEdit, QGridLayout, QStackedLayout
 from PyQt6 import QtGui, QtCore
+from PyQt6.QtCore import QRunnable, pyqtSlot, QThreadPool, Qt
 
-import sys
+import speech
+import sys, time
+from datetime import datetime
+
+# Trip computer. Updates distance traveled every second. Currently moving at 60 km/h
+class tripThread(QRunnable):
+    @pyqtSlot()
+    def run(self):
+        tripDistance = 0
+        speed = 60
+        timeCount = 0
+        while True:
+            time.sleep(1)
+            timeCount = datetime.now()
+            tripDistance = tripDistance + ((speed / 60 )/ 60)
+            window.tripDistanceLabel.setText("Distance travelled: " + str(round(tripDistance,2)) + "km")
+            window.speedLabel.setText("Speed: " + str(speed) + "km/h")
+            window.timeLabel.setText("Trip Time: " + str(timeCount.hour) + ":" + str(timeCount.minute) + ":" + str(timeCount.second))
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # Define the stylesheet for the windows
+        with open("styles.css", "r") as file:
+            self.setStyleSheet(file.read())
+        self.threadPool = QThreadPool()
 
         self.setWindowTitle("Really early version")
     
@@ -19,19 +41,14 @@ class MainWindow(QMainWindow):
         self.stackLayout = QStackedLayout()
 
         # Define each button for the home screen
-        button1 = self.define_button("images/radio.png", 100,100,self.change_screen, 1)
-        button2 = self.define_button("images/maps.png",100,100,self.change_screen,2)
-        button3 = self.define_button("images/tripcomputer.png",100,100,self.change_screen,3)
-        button4 = self.define_button("images/phone.png",100,100,self.change_screen,4)
-        button5 = self.define_button("images/messages.png",100,100,self.change_screen,5)
-        button6 = self.define_button("images/bluetooth.png",100,100,self.change_screen,6)
-        button7 = self.define_button("images/seat.png",100,100,self.change_screen,7)
-        button8 = self.define_button("images/settings.png",100,100,self.change_screen,8)
-
-        # Create the textField for "voice" input. Jie probably has something better than this already.
-        self.textField = QLineEdit()
-        self.textField.setText("A surrogate for voice input. Try deleting this and replacing with 'radio' or 'phone'")
-        self.textField.textChanged.connect(self.commandSearch)
+        button1 = self.define_button("images/radio.png", 200,200,self.change_screen, 1)
+        button2 = self.define_button("images/maps.png",200,200,self.change_screen,2)
+        button3 = self.define_button("images/tripcomputer.png",200,200,self.change_screen,3)
+        button4 = self.define_button("images/phone.png",200,200,self.change_screen,4)
+        button5 = self.define_button("images/messages.png",200,200,self.change_screen,5)
+        button6 = self.define_button("images/bluetooth.png",200,200,self.change_screen,6)
+        button7 = self.define_button("images/seat.png",200,200,self.change_screen,7)
+        button8 = self.define_button("images/settings.png",200,200,self.change_screen,8)
 
         # Add the home screen buttons to the home screen with grid coordinates
         homeScreenLayout.addWidget(button1, 0, 0)
@@ -42,18 +59,52 @@ class MainWindow(QMainWindow):
         homeScreenLayout.addWidget(button6, 1, 1)
         homeScreenLayout.addWidget(button7, 1, 2)
         homeScreenLayout.addWidget(button8, 1, 3)
-        homeScreenLayout.addWidget(self.textField, 2, 0)
 
         # Create the "go home" button
         homeButton = QPushButton("Back to main screen")
         homeButton.clicked.connect(lambda: self.change_screen(0))
 
+        # Create the radio menu and associated buttons
+        radioContainer = QWidget()
+        radioGridContainer = QWidget()
+        radioList = QHBoxLayout(radioContainer)
+        radioMenu = QGridLayout(radioGridContainer)
+        self.radioLabel = QLabel("Now listening to:")
+        self.radioLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        radioButton1 = self.define_button("RTE 1", 200,60,self.change_station, "RTE 1")
+        radioButton2 = self.define_button("RTE 2", 200,60,self.change_station, "RTE 2")
+        radioButton3 = self.define_button("Newstalk", 200,60,self.change_station, "Newstalk")
+        radioButton4 = self.define_button("SPIN SW", 200,60,self.change_station, "SPIN SW")
+        radioList.addWidget(radioButton1)
+        radioList.addWidget(radioButton2)
+        radioList.addWidget(radioButton3)
+        radioList.addWidget(radioButton4)
+        radioMenu.addWidget(self.radioLabel)
+        radioMenu.addWidget(radioContainer)
+        radioBack = self.define_button("Back", 160, 40, self.change_screen, 0)
+        radioMenu.addWidget(radioBack)
+
+        #Create the trip computer menu
+        tripContainer = QWidget()
+        tripMenu = QVBoxLayout(tripContainer)
+        self.tripDistanceLabel = QLabel("")
+        self.speedLabel = QLabel("")
+        self.timeLabel = QLabel("")
+        self.tripDistanceLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.speedLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.timeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        tripMenu.addWidget(self.timeLabel)
+        tripMenu.addWidget(self.speedLabel)
+        # tripMenu.addWidget(self.tripDistanceLabel)
+        tripBack = self.define_button("Back", 160, 40, self.change_screen, 0) # Cannot copy a button, have to create a new one for each menu. I know.
+        tripMenu.addWidget(tripBack)
+
         # Add the different main screens to the stack layout (main window that will change when something happens)
         self.stackLayout.addWidget(container)
+        self.stackLayout.addWidget(radioGridContainer)
         self.stackLayout.addWidget(homeButton)
-        # The next 7 are just for testing purposes, so each button does something.
-        self.stackLayout.addWidget(homeButton)
-        self.stackLayout.addWidget(homeButton)
+        self.stackLayout.addWidget(tripContainer)
+        # The next 5 are just for testing purposes, so each button does something.
         self.stackLayout.addWidget(homeButton)
         self.stackLayout.addWidget(homeButton)
         self.stackLayout.addWidget(homeButton)
@@ -62,48 +113,56 @@ class MainWindow(QMainWindow):
 
         # Add the stack layout and text input to the main menu
         layout.addLayout(self.stackLayout, 0, 0)
-        #layout.addLayout(sliderLayout,0,1 )
-        layout.addWidget(self.textField,1,0)
 
         # Define the main widget for every visible element
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
-        self.setFixedHeight(300)
-        self.setFixedWidth(500)
+        self.setFixedHeight(450)
+        self.setFixedWidth(900)
+
+        # Create and start running the speech to text and trip computer threads 
+        speechT = self.speechThread()
+        tripT = tripThread()
+        self.threadPool.start(speechT)
+        self.threadPool.start(tripT)
 
     # Swap screens to specified index
     def change_screen(self, index):
-        self.textField.setText("")
         self.stackLayout.setCurrentIndex(index)
 
-
-    # Create a button with an icon, width, height and connect it to a different screen
-    def define_button(self, icon, w, h, screen, index):
-        button = QPushButton()
-        button.setIcon(QtGui.QIcon(icon))
-        button.setIconSize(QtCore.QSize(w,h))
-        button.setFixedSize(QtCore.QSize(w,h))
-        button.clicked.connect(lambda: screen(index))
-        return button
+    # Change radio stations
+    def change_station(self,station):
+        self.radioLabel.setText("Now listening to: " + station)
     
-    # Search for a command based on text input. 
-    def commandSearch(self):
-        command = self.textField.text()
-        command = command.lower()
+    # Create a button with an icon or text field, width, height and connect it to a method that needs an argument
+    def define_button(self, iconOrText, w, h, method, methodArg):
+        button = QPushButton()
+        if ".png" in iconOrText: 
+            button.setIcon(QtGui.QIcon(iconOrText))
+            button.setIconSize(QtCore.QSize(w,h))
+        else:
+            button.setText(iconOrText)
+        button.setFixedSize(QtCore.QSize(w+2,h+2))
+        button.clicked.connect(lambda: method(methodArg))
+        return button
+      
+    # Thread class to run the speech recognition in the background
+    class speechThread(QRunnable):
+        @pyqtSlot()
+        def run(self):
+            while True:
+                text = speech.main()
+                if (type(text) is int):
+                    window.change_screen(text)
+                else:
+                    window.change_station(text)
 
-        # Each command has an index number for the stack layout. If a command exists, swap to the screen that matches said index.
-        commands = {"back":0, "radio": 1, "maps":2, "trip computer":3, "phone":4, "messages":5, 
-                    "bluetooth":6, "seat":7, "settings":8}
-        for entry in commands: 
-            if (command == entry):
-                index = commands.get(command)
-                self.change_screen(index)
-        
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 
 app.exec()
+
 
 
--- speech.py
+++ speech.py
@@ -40,8 +40,6 @@ def set_volume(value):
     value = max(0, min(100, value))
     volume_interface.SetMasterVolumeLevelScalar(value / 100.0, None)
 
-# Wake control
-awake = False
 
 def listen():
     with mic as source:
@@ -50,88 +48,109 @@ def listen():
     return recognizer.recognize_google(audio).lower()
 
 # Main loop
-print("Say 'Hi Jaguar' to activate.")
+def main():
+    # Wake control
+    awake = False
+    print("Say 'Hi Jaguar' to activate.")
 
-while True:
-    try:
-        print("Listening...")
-        text = listen()
-        print("You said:", text)
+    while True:
+        try:
+            print("Listening...")
+            text = input() #listen()
+            print("You said:", text)
 
-        if not awake:
-            if "hi jaguar" in text:
-                awake = True
-                speak("Yes?")
-            continue
+            if not awake:
+                if "hi jaguar" in text:
+                    awake = True
+                    speak("Yes?")
+                continue
 
-        # Sleep command
-        if "go to sleep" in text:
-            speak("Going to sleep.")
-            awake = False
-            continue
+            # Sleep command
+            if "go to sleep" in text:
+                speak("Going to sleep.")
+                awake = False
+                continue
 
-        # Volume commands
-        if "volume" in text:
-            if "increase" in text:
-                current = volume_interface.GetMasterVolumeLevelScalar() * 100
-                set_volume(int(current + 10))
-                speak("Volume increased.")
-            elif "decrease" in text:
-                current = volume_interface.GetMasterVolumeLevelScalar() * 100
-                set_volume(int(current - 10))
-                speak("Volume decreased.")
-            elif "set" in text:
-                match = re.search(r"(\d+)", text)
-                if match:
-                    set_volume(int(match.group(1)))
-                    speak("Volume set.")
-            awake = False
-            continue
+            # Volume commands
+            if "volume" in text:
+                if "increase" in text:
+                    current = volume_interface.GetMasterVolumeLevelScalar() * 100
+                    set_volume(int(current + 10))
+                    speak("Volume increased.")
+                elif "decrease" in text:
+                    current = volume_interface.GetMasterVolumeLevelScalar() * 100
+                    set_volume(int(current - 10))
+                    speak("Volume decreased.")
+                elif "set" in text:
+                    match = re.search(r"(\d+)", text)
+                    if match:
+                        set_volume(int(match.group(1)))
+                        speak("Volume set.")
+                awake = False
+                continue
 
-        # Brightness commands
-        if "brightness" in text:
-            if "increase" in text:
-                sbc.set_brightness("+=10")
-                speak("Brightness increased.")
-            elif "decrease" in text:
-                sbc.set_brightness("-=10")
-                speak("Brightness decreased.")
-            elif "set" in text:
-                match = re.search(r"(\d+)", text)
-                if match:
-                    sbc.set_brightness(int(match.group(1)))
-                    speak("Brightness set.")
-            awake = False
-            continue
+            # Brightness commands
+            if "brightness" in text:
+                if "increase" in text:
+                    sbc.set_brightness("+=10")
+                    speak("Brightness increased.")
+                elif "decrease" in text:
+                    sbc.set_brightness("-=10")
+                    speak("Brightness decreased.")
+                elif "set" in text:
+                    match = re.search(r"(\d+)", text)
+                    if match:
+                        sbc.set_brightness(int(match.group(1)))
+                        speak("Brightness set.")
+                awake = False
+                continue
 
-        # Maps
-        if "open maps" in text or "navigate" in text:
-            speak("Starting point?")
-            start = listen()
-            speak("Destination?")
-            end = listen()
-            url = f"https://www.google.com/maps/dir/{start}/{end}"
-            webbrowser.open(url)
-            speak("Opened Google Maps.")
-            awake = False
-            continue
+            # Maps
+            if "open maps" in text or "navigate" in text:
+                speak("Starting point?")
+                start = input() #listen()
+                speak("Destination?")
+                end = input() #listen()
+                url = f"https://www.google.com/maps/dir/{start}/{end}"
+                webbrowser.open(url)
+                speak("Opened Google Maps.")
+                awake = False
+                continue
 
-        # Telegram messaging
-        if "send message" in text or "telegram" in text:
-            speak("What should I send?")
-            message = listen()
-            print(f"[Mock] Would send this message via Telegram: {message}")
-            speak("Message saved for Telegram demo.")
-            with open("demo_mock_messages.txt", "a") as f:
-                f.write(f"{message}\n")
-            awake = False
-            continue
+            # Telegram messaging
+            if "send message" in text or "telegram" in text:
+                speak("What should I send?")
+                message = input() #listen()
+                print(f"[Mock] Would send this message via Telegram: {message}")
+                speak("Message saved for Telegram demo.")
+                with open("demo_mock_messages.txt", "a") as f:
+                    f.write(f"{message}\n")
+                awake = False
+                continue
 
-        # Unknown
-        speak("Command not recognized.")
-        awake = False
+            # GUI Switches
+            commands = {"back":0, "home":0, "return":0, "radio": 1, "map":2, "nav":2, "satnav":2, "trip":3, "phone":4, "message":5, 
+                    "bluetooth":6, "seat":7, "settings":8}
+            for entry in commands: 
+                if (entry == text):
+                    awake = False
+                    return commands.get(entry)
+                
+            # Radio Stations
+            stations = {"rte one": "RTE 1", "rte two": "RTE 2", "newstalk": "NEWSTALK", "spin": "SPIN SW"}
+            for entry in stations:
+                if (entry in text):
+                    awake = False
+                    return stations.get(entry)
+                
+            
+            # Unknown
+            speak("Command not recognized. Please try again.")
 
-    except sr.UnknownValueError:
-        print("Could not understand.")
-    except Exception as e:
-        print("Error:", e)
+            
+
+        except sr.UnknownValueError:
+            print("Could not understand.")
+        except Exception as e:
+            print("Error:", e)
+
